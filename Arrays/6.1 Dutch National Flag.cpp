931a2e890438a6d79f0f8499b5c1b9bd9cbaf3f4
Koushik Cherukuri <Koushikch2002@gmail.com>
2022-07-07 09:06:08 +0530

    Fixed code

--- Arrays/6.1 Dutch National Flag.cpp
+++ Arrays/6.1 Dutch National Flag.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -62,6 +61,29 @@ void better(vector<int> &nums, int pivot)
 
 void optimal(vector<int> &nums, int pivot)
 {
+
+    /*    The time complexity is0(n) and the space complexity is0(1).
+        The algorithm we now present is similar to the one sketched above. The main
+        difference is that it performs classification into elements less than, equal to, and
+        greater than the pivot in a single pass. This reduces runtime, at the cost of a trickier
+        implementation. We do this by maintaining four subarrays: bottom (elements less
+        than pivot), middle (elements equal to pivot), unclassified, and top (elements greater
+        than pivot). Initially, all elements are in unclassified. We iterate through elements in
+        unclassified, and move elements into one of bottom, middle, and top groups according
+        to the relative order between the incoming unclassified element and the pivot.
+        Asa concrete example,suppose thearray iscurrentlyA = (-3,0,-1,1,1, ?,?,?,4, 2),
+        where the pivot is1and ? denotes unclassified elements. There are three possibilities
+        for the first unclassified element, A[5].
+
+        • A[5] is less than the pivot, e.g., A[5] = -5. We exchange it with the first 1, i.e.,
+        the new array is(-3,0,-1,-5,1,1, ?,?,4, 2).
+        • A[5] is equal to the pivot, i.e., A[5] = 1. We do not need to move it, we just ad¬
+        vance to the next unclassified element, i.e., the array is(-3,0,-1,1,1,1, ?, ?, 4, 2).
+        • A[5] is greater than the pivot, e.g., A[5] = 3. We exchange it with the last
+        unclassified element, i.e., the new array is(-3,0,-1,1,1, ?, ?,3,4, 2).
+
+    */
+
     // TC->O(N)
 
     int lowPtr = 0, highPtr = nums.size() - 1, i = 0;
@@ -83,6 +105,11 @@ void optimal(vector<int> &nums, int pivot)
     for (auto num : nums)
         cout << " " << num;
 }
+
+/* Variants:
+
+
+*/
 
 int main()
 {
